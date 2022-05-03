31c7fc2841d1c07a111a40dd46f2e9d0e0e2f197
Mihai-Valentin DUMITRU <mihai.dumitru@cs.pub.ro>
2022-05-03 20:50:02 +0300

    debug commit 2

--- vmck/checker.sh
+++ vmck/checker.sh
@@ -4,7 +4,7 @@ unzip archive.zip
 unzip -qq -o artifact.zip
 
 ghc --make Tasks.hs
-#find . -regex ".*\.o$" | xargs ghc --make -o main
+find . -regex ".*\.o$" | xargs ghc --make -o main
 #ghc --make main.hs
 #./main
 
