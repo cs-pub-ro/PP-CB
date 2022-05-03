eecc8b643eca0c36d6026004718c580c797b1b40
Mihai-Valentin DUMITRU <mihai.dumitru2201@upb.ro>
2022-05-03 20:16:51 +0300

    precompile less files

--- vmck/checker.sh
+++ vmck/checker.sh
@@ -4,7 +4,8 @@ unzip archive.zip
 unzip -qq -o artifact.zip
 
 ghc --make Tasks.hs
-find . -regex ".*\.o$" | xargs ghc --make -o main
+#find . -regex ".*\.o$" | xargs ghc --make -o main
+ghc --make main.hs
 ./main
 
 exit $?
