33ee005f76603d47c5bb0417930419a576a49f05
Mihai-Valentin DUMITRU <mihai.dumitru@cs.pub.ro>
2022-05-05 17:46:16 +0300

    ?

--- vmck/checker.sh
+++ vmck/checker.sh
@@ -1,14 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash
 
 unzip archive.zip
 unzip -qq -o artifact.zip
-
-free -h
-
-ghc --make Tasks.hs
-find . -regex ".*\.o$" | xargs ghc --make -o main
-timeout 5 ghc --make main.hs
-echo "here"
-./main
-
+runghc main.hs
 exit $?
