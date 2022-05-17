8b1260c7db8e39d102f222a99f19e4ad8058034c
Mihai-Valentin DUMITRU <mihai.dumitru2201@upb.ro>
2022-05-17 18:14:22 +0300

    update all stages

--- vmck/checker.sh
+++ vmck/checker.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 unzip archive.zip
-unzip -qq -o artifact.zip
+
+
+echo "*** ETAPA 1 ***"
+unzip -qq -o artifact1.zip
 runghc main.hs
+
+echo "*** ETAPA 2 ***"
+unzip -qq -o artifact2.zip
+runghc main.hs
+
+echo "*** ETAPA 3 ***"
+unzip -qq -o artifact3.zip
+runghc main.hs
+
 exit $?
